fb944e56c7ba99801c5342f96d626f484e6eb5d7
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-11-17 13:50:41 -0800

    Modify server to export app

--- server/server.js
+++ server/server.js
@@ -3,15 +3,14 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname +'/views');
+app.set('views', __dirname +'app/views');
 app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
-
 app.get('/', routes.index);
-app.get('/partials/:name', routes.partials);
- 
+
 
 app.get('*', routes.index);
 
-var server = app.start();
+
+module.exports = app;
+
