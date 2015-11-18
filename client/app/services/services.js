a41c177fd338394535f444f719f3131910c521b1
Justin Zimmerman <zimmy130@gmail.com>
2015-11-18 14:48:10 -0500

    Add angular services and routing
    add angular routing dependency

---
+++ client/app/services/services.js
@@ -0,0 +1,26 @@
+angular.module('app.services', [])
+  .factory('Venues', function($http) {
+
+    var getVenues = function() {
+      return $http({
+        method: 'GET',
+        url: '/api/venues'
+      })
+      .then(function(res) {
+        return res.data;
+      });
+    };
+
+    var addVenue = function(data) {
+      return $http({
+        method: 'POST',
+        url: '/api/venues',
+        data: data
+      });
+    };
+
+    return {
+      getVenues: getVenues,
+      addVenue: addVenue
+    };
+  });
